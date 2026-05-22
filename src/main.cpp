e724138ba67fd8b56d7518662dc72d2c360506b4
diegoandrebraz-design <diegoandrebraz@aluno.ueg.br>
2026-05-21 22:03:00 -0300

    Atualizar o main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,32 @@
 #include <opencv2/opencv.hpp>
 #include "../include/render.h"
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
     std::string entrada;
     int opcao;
 
-std::cout << "Resgate. Restaure. Relembre" << std::endl;
-std::cout << "[1] Abrir um arquivo de mídia" << std::endl;
-std::Cout << "[2] Sincronizar a Webcam" << std::endl;
-std::cout << "Escolha uma opção, digite 1 ou 2" << std::endl;
+    std::cout << "RESGATE. RESTAURE. RELEMBRE" << std::endl;
+    
+    std::cout << "[1] Abrir um arquivo de mídia" << std::endl;
+    std::cout << "[2] Sincronizar a Webcam" << std::endl;
+    std::cout << "Opção: ";
+    std::cin >> opcao;
 
-std::cout << "Opção: ";
-std::cin >> opcao;
-
-if opcao == 1 {
-    std::cout << "Informe o caminho do arquivo: ";
-    std::cin >> entrada;
-}
-    else if opcao == 2 {
-    entrada = "0";
-    std::cout << "Acessando a Webcam..." << std::endl;
-}
-    else {
-    std::cout << "Opção inválida" << std::endl;
-
-return -1;
+    if (opcao == 1) {
+        std::cout << "Informe o caminho do arquivo: ";
+        std::cin >> entrada;
+    } else if (opcao == 2) {
+        entrada = "0"; 
+        std::cout << "Acessando a Webcam..." << std::endl;
+    } else {
+        std::cout << "Opção inválida." << std::endl;
+        return -1;
     }
 
-Render processador(entrada);
+    Render processador(entrada);
 
-std::cout << "Processador de Mídia" << std::endl;
-std::cout << "Escolha o filtro desejado: " << std::endl;
-    std::cout << "1. Girar e Inverter" << std::endl;
+    std::cout << "\n ESCOLHA O FILTRO " << std::endl;
+   std::cout << "1. Girar e Inverter" << std::endl;
     std::cout << "2. Recortar" << std::endl;
     std::cout << "3. Desfocar" << std::endl;
     std::cout << "4. Reduzir Ruídos" << std::endl;
@@ -42,13 +37,23 @@ std::cout << "Escolha o filtro desejado: " << std::endl;
     std::cout << "8. Correção de Cores" << std::endl;
     std::cout << "9. Realçar Nitidez" << std::endl;
     std::cout << "10. Tons de Cinza" << std::endl;
+    
+    int escolha;
+    std::cout << "Digite o número do filtro: ";
+    std::cin >> escolha;
 
-int escolha;
-std::cin >> escolha;
+    double alfa = 50.0;
+    float beta = 50.0f;
+    int gama = 1;
+    int delta = 1;
 
-double alfa = 50.0, beta = 50.0;
+    if (escolha == 2) {
+        std::cout << "Configuração de recorte (Gama=Largura, Delta=Altura):" << std::endl;
+        std::cout << "Largura: "; std::cin >> gama;
+        std::cout << "Altura: "; std::cin >> delta;
+    }
 
-processador.render(cv::Mat(), escolha, alfa, beta, gama, delta);
+    processador.render(cv::Mat(), escolha, alfa, beta, gama, delta);
 
-return o;
+    return 0;
 }
